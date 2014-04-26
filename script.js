a7dfb4db51670c70088aedf40e1130b584144b81
iyengarrishi <ri2195@columbia.edu>
2014-04-25 23:11:04 -0400

    Split URL, added click function with e and changed URL with history.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 /**
  * @author
  */
+
+//Split the API key and table ID URLs using variables
+
+var tableKey="&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU"
+
+var tableURL="https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1v4nYSIkAHDXen3wswKczthKQRu4w5uFO3oUxupMs+WHERE+DATE>="
+
 function loadData(UNEMPDATA) {
 
 	console.log(UNEMPDATA);
@@ -28,9 +35,36 @@ function loadData(UNEMPDATA) {
 
 }
 
-
+//Create a click handler using e
+function changeData(e){
+	var tableID = e.target.id;
+	console.log(tableID);
+	var tableArray = tableID.split("_");//This separates the years from the dates
+	var tableYear = tableArray[1];//This identifies the year we are selecting on click
+	$.get(tableURL+"'"+tableYear+"-01-01'"+tableKey, loadData, "json");//This get request changes the data according to the button clicked
+	
+	//I notice at this point that my page loads blank and the chart appears only when I click
+	//So I need to set a default year that will open on the page
+	
+	//Bring in History JS to split the URL and change it
+	History.pushState({year:tableYear}, "Unemployment from -"+tableYear, "?year="+tableYear);
+}
 
 function loadGoogle() {
+	
+	var yearURL = History.getState().cleanUrl;
+	var tableArray = yearURL.split("?"); //Splitting the URL on the question mark
+	
+	var defaultYear = "1980";
+
+	
+	if(tableArray.length > 1){
+		//get the query string, break it on equals and 
+		defaultYear = tableArray[1].split("=")[1];
+	}
+	
+	
+	$(".btn-success").on("click", changeData);
 
 	console.log("Loaded Google Viz");
 
@@ -38,7 +72,7 @@ function loadGoogle() {
 	//Instead of loading data from a static JSON file, I will load it from a Google Fusion Table
 
 
-	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1v4nYSIkAHDXen3wswKczthKQRu4w5uFO3oUxupMs+WHERE+DATE>='1979-12-01'&key=AIzaSyCzc9XKi4CG-PcqBZfHBmc3fKmuq3JH9vU", loadData, "json");
+	
 
 }
 
